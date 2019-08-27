148c741067e3720b12d8f3b467a9482598b80940
Antonio Carlos da Silva <acdasilva@gmail.com>
2019-08-27 20:41:24 -0300

    Com Invoke

--- PipeLineCoreProj/Startup.cs
+++ PipeLineCoreProj/Startup.cs
@@ -20,9 +20,14 @@ namespace PipeLineCoreProj
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Gerado pelo Pipeline");
+                await next.Invoke();
+            });
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello MVC!");
             });
         }
     }
